bf10c26281b214f62e5fcb9bd03f5aba295e98a5
GwonHyeok <kh4975@naver.com>
2014-03-06 11:31:43 +0100

    XuiMod: Toast Animations [2/2]

--- src/com/kcuf/settings/fragments/AnimationSettings.java
+++ src/com/kcuf/settings/fragments/AnimationSettings.java
@@ -16,7 +16,6 @@
 
 package com.kcuf.settings.fragments;
 
-import android.content.Context;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.UserInfo;
@@ -30,6 +29,7 @@ import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
+import android.widget.Toast;
 import com.android.settings.R;
 import android.support.annotation.NonNull;
 
@@ -44,6 +44,11 @@ import java.util.List;
 public class AnimationSettings extends SettingsPreferenceFragment
                 implements Preference.OnPreferenceChangeListener {
 
+    private static final String KEY_TOAST_ANIMATION = "toast_animation";
+
+    private ListPreference mToastAnimation;
+
+    Toast mToast;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -52,12 +57,42 @@ public class AnimationSettings extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.kcuf_settings_animations);
 
         final ContentResolver resolver = getActivity().getContentResolver();
-        final PreferenceScreen prefScreen = getPreferenceScreen();
 
+        mToastAnimation = (ListPreference) findPreference(KEY_TOAST_ANIMATION);
+        int toastanimation = Settings.System.getIntForUser(resolver,
+                Settings.System.TOAST_ANIMATION, 1,
+                UserHandle.USER_CURRENT);
+        mToastAnimation.setValue(String.valueOf(toastanimation));
+        mToastAnimation.setSummary(mToastAnimation.getEntry());
+        int CurrentToastAnimation = Settings.System.getInt(getContentResolver(), Settings.System.TOAST_ANIMATION, 1);
+        mToastAnimation.setValueIndex(CurrentToastAnimation); //set to index of default value
+        mToastAnimation.setSummary(mToastAnimation.getEntries()[CurrentToastAnimation]);
+        mToastAnimation.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mToastAnimation) {
+            int value = Integer.parseInt((String) newValue);
+            int index = mToastAnimation.findIndexOfValue((String) newValue);
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.TOAST_ANIMATION, value, UserHandle.USER_CURRENT);
+            mToastAnimation.setSummary(mToastAnimation.getEntries()[index]);
+            if (mToast != null) {
+                mToast.cancel();
+            }
+            mToast = Toast.makeText(getActivity(), R.string.toast_animation_test,
+                    Toast.LENGTH_SHORT);
+            mToast.show();
+            return true;
+
+        }
 
         return false;
     }
@@ -66,5 +101,4 @@ public class AnimationSettings extends SettingsPreferenceFragment
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.KCUF_SETTINGS;
     }
-
 }
